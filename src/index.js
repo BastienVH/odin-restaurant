ca13def74899c91048a3cb59ef560cc41043fec3
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-08-11 22:11:57 +0200

    add logo to page with js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const content = document.getElementById('content');
+import Logo from './logo.png';
 
 function header() {
   const header = document.createElement('div');
+
+  // add header logo
+  const myLogo = new Image();
+  myLogo.src = Logo;
+  myLogo.id = 'logo';
+  header.appendChild(myLogo);
 
   // add header text
   const headerTxt = document.createElement('h1');
