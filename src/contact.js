f82dc937572dddcef74156a7fe274151b58471c9
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-09-03 21:08:18 +0200

    Update text for contact page

--- src/contact.js
+++ src/contact.js
@@ -3,12 +3,16 @@ function contact() {
   element.classList.add('content');
 
   const para1 = document.createElement('p');
-  para1.innerText = 'Here you can find the contact details of our friterie.';
+  para1.innerText = 'Want to place an order or meet us in our friterie?\n You\'ll find all of the deets below!';
   element.appendChild(para1);
   
   const p2 = document.createElement('p');
-  p2.innerText = 'Come grab a bite to eat at one of our tables, or take your order to go.\nEither way you\'ll find us at 211 E River Street, Antwerp, OH 45813.'
+  p2.innerText = 'You can find us at 211 E River Street, Antwerp, OH 45813.'
   element.appendChild(p2);
+
+  const p3 = document.createElement('p');
+  p3.innerHTML = 'Send us an email at <strong>frietjes@thefryshackisdefinitelyarealrestaurant.com</strong>.';
+  element.appendChild(p3);
 
   return element;
 }
